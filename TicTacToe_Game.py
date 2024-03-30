09ae9e9826eb80ddbf11673aee82ccc97330c176
PanickingRatAB <ainhoabilbao2008@gmail.com>
2024-03-30 11:52:40 +0800

    done

--- TicTacToe_Game.py
+++ TicTacToe_Game.py
@@ -13,7 +13,6 @@ class Move(NamedTuple):
     row: int
     col: int
     label: str = ""
-
     BOARD_SIZE = 9
     DEFAULT_PLAYERS = (
         Player(label="X", color="blue"),
@@ -21,21 +20,41 @@ class Move(NamedTuple):
     )
 
 class TicTacToe_Logic:
-    def __init__(self, players=DEFAULT_PLAYERS, board_size=BOARD_SIZE):
+    def __init__(self, players = DEFAULT_PLAYERS, board_size = BOARD_SIZE):
         self._players = cycle(players)
         self.board_size = board_size
         self.current_player = next(self._players)
+        self.winner_combo = []
         self._current_moves = []
         self._has_winner = False
         self._setup_board()
 
+    def _setup_board(self):
+        self._current_moves = [
+            [Move(row, col) for col in range(self.board_size)]
+            for row in range(self.board_size)]
+        
+    def valid_move(self, move):
+        row, col = move.row, move.col
+        move_was_not_played = self._current_moves[row][col].label == ""
+        no_winner = not self._has_winner
+        return no_winner and move_was_not_played
+    
+    def toggle_player(self):
+        self.current_player = next(self._players)
+
 class TicTacToe_Board(tk.Tk):
-    def __init__(self):
+    def __init__(self, game):
         super().__init__()
         self.title("TicTacToe 2.0")
         self._cells = {}
+        self._game = game
         self._create_board_display()
         self._create_board_grid()
+
+    def _update_button(self, clicked_btn):
+        clicked_btn.config(text=self._game.current_player.label)
+        clicked_btn.config(fg=self._game.current_player.color)
 
     def _create_board_display(self):
         display_frame = tk.Frame(master=self)
@@ -50,16 +69,28 @@ class TicTacToe_Board(tk.Tk):
         grid_frame = tk.Frame(master=self)
         grid_frame.pack()
         
-        for row in range(9):
+        for row in range(self._game.board_size):
             self.rowconfigure(row, weight=1, minsize=50)
             self.columnconfigure(row, weight=1, minsize=50)
-            for col in range(9):
+            for col in range(self._game.board_size):
                 button = tk.Button(master=grid_frame, text="", font=font.Font(size=5, weight="bold"), width=8, height=5)
                 self._cells[button] = (row, col)
                 button.grid(row=row, column=col)
 
+                self._cells[button] = (row, col)
+                button.bind("<ButtonPress-1>", self.play)
+
+    def play(self, event):
+        clicked_btn = event.widget
+        row, col = self._cells[clicked_btn]
+        move = Move(row, col, self._game.current_player.label)
+        if self._game.is_valid_move(move):
+            self._update_button(clicked_btn)
+            self._game.process_move(move)
+            self._game.toggle_player()
+
 def main():
-    board = TicTacToe_Game()
+    board = TicTacToe_Board()
     board.mainloop()
 
 if __name__ == "__main__":
